440f95cb4e0307610aa75966c60a503e1e49f457
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-09-21 09:54:23 +0530

    added user and question schemas

---
+++ backend/models/Question.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Question = new Schema({
+    level: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String   
+    },
+    video: {
+        type: String   
+    },
+    audio: {
+        type: String   
+    },
+    questionText: {
+        type: String,
+        default: ""
+    },
+    urlHint: {
+        type: String,
+        required: true
+    },
+    hints: [{
+        isImage: Boolean,
+        isVideo: Boolean,
+        isAudio: Boolean,
+        hint: String
+    }],
+    points: {
+        type: Number
+    }
+});
+
+module.exports = question = mongoose.model("Question", Question);
---
+++ backend/models/User.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const nconf = require('nconf');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String,
+    unique: true,
+    required: true,
+    minLength: 4,
+    maxLength: 10
+  },
+  userAvatar: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  college: {
+    type: String,
+    required: true
+  },
+  registrationNum: {
+    type: String, 
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 10
+  },
+  score: {
+    type: Number,
+    default: 0
+  },
+  currLevel: {
+    type: Number,
+    default: 0
+  }, 
+  // for forgot password functionality
+  verifyCode: {
+    type: String
+  },
+  answerLog: [{
+    level: Number,
+    userAnswer: String,
+    isCorrect: Boolean
+  }]
+});
+
+// create a jwt with the user payload
+User.methods.generateAuthToken = function(payload) {
+    token = jwt.sign(
+        payload,
+        nconf.get('JWT_SECRET'),
+        { expiresIn: 360000 }
+    );
+    return token;team
+}
+
+// encrypts the password before saving to the database
+User.pre('save', function (next) {
+    var user = this;
+  
+    if (user.isModified('password')) {
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+          user.password = hash;
+          next();
+        });
+      });
+    } else {
+      next();
+    }
+});
+
+module.exports = user = mongoose.model("User", User);
