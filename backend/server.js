dd6d898269bb757c4a47c9f4f427be1daacc069c
Ayush Pandey <73249644+heisenberg-ayush@users.noreply.github.com>
2021-09-27 10:54:55 +0530

    Pass-reset__sendEmail

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const nodemailer = require('nodemailer');
+require("dotenv").config();
+
 const connectDB = require("./utils/connectDb");
 
 const port = process.env.PORT || 3002;
@@ -20,6 +23,41 @@ app.use("/api/areyouhuman", require("./routes/api/recaptcha"));
 
 // routes for everything related to the game
 app.use("/api/game", require("./routes/api/play"));
+
+// routes for password reset
+app.post('/send', async(req, res) => {
+    const output = `
+    <p>You have a new password reset request</p>
+    <p>Email: ${req.body.email}</p>
+    `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: process.env.HOST,
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL, // generated ethereal user
+            pass: process.env.PASS, // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: `"Smoked Contact 👻" <${process.env.EMAIL}>`, // sender address
+        to: req.body.email, // list of receivers
+        subject: "Password Reset Request", // Subject line
+        text: "", // plain text body
+        html: output, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // res.render('password', { msg: 'Email has been sent' });
+});
 
 // listens to the app on PORT
 app.listen(port, () => {
