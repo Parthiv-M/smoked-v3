56f79db500e72e786eab33ef4881950f2fcf5204
Adilk6 <75502870+Adilk6@users.noreply.github.com>
2021-10-21 01:12:41 +0530

    Add files via upload

---
+++ client/src/Sidenav/pages/link2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function link2() {
+  return (
+    <div className='link3'>
+      <h3>Link 2</h3>
+    </div>
+  );
+}
+
+export default link2;
---
+++ client/src/Sidenav/pages/link3.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function link3() {
+  return (
+    <div className='link3'>
+      <h3>Link 3</h3>
+    </div>
+  );
+}
+
+export default link3;
